372a0ea1455490ed894eaa26db188152961ac78e
greyes97 <greyesl1@miumg.edu.gt>
2021-04-25 21:59:35 -0600

    Files of the project added

---
+++ src/main/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/parcial2/shell_sort/ShellSortApplication.java
@@ -0,0 +1,17 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.parcial2.shell_sort;
+
+import gt.edu.umg.ingenieria.sistemas.ingenieria_software.parcial2.shell_sort.model.ShellSort;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ShellSortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShellSortApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gt/edu/umg/ingenieria/sistemas/ingenieria_software/parcial2/shell_sort/ShellSortApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.ingenieria.sistemas.ingenieria_software.parcial2.shell_sort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellSortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
